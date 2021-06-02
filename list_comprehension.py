cf231ad1436fe10bd8d7239ece177d9a4a35fb16
Mauro Leonelli <mleonelli@gmail.com>
2021-06-02 12:01:29 +0000

    Added 2 list comprehensions to the set

--- list_comprehension.py
+++ list_comprehension.py
@@ -6,3 +6,10 @@ def double_list_comprehension_range(n, m):
 
 def byte_sizes():
   return [2 ** n for n in range(3, 20)]
+
+def order_list():
+  earners = {'A' : 200000, 'B' : 90000, 'C' : 300000}
+  return [(k,v) for v,k in sorted([ (v,k) for k, v in earners.items() if v >= 100000 ], reverse=True)]
+
+def open_file(file_name):
+  return [line.strip() for line in open(file_name)]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import list_comprehension as lc
 from unittest import main
 
+"""
 # Create a list of all the integers from 0 to n-1
 print("Create a list of all the integers from 0 to n-1")
 print("[x for x in range(n)]")
@@ -18,6 +19,20 @@ print("Random fun with bytes sizes (powers of 2 from 3 to 20)")
 print("[2 ** n for n in range(3, 20)]")
 print(lc.byte_sizes())
 print("\n")
+
+
+# Return a filtered ordered list
+print("Return a filtered ordered list")
+print("return [(k,v) for v,k in sorted([ (v,k) for k, v in earners.items() if v >= 100000 ], reverse=True)]")
+print(lc.order_list())
+print("\n")
+
+# Open a file and return stripped content
+print("Open a file and return stripped content")
+print("return [(k,v) for v,k in sorted([ (v,k) for k, v in earners.items() if v >= 100000 ], reverse=True)]")
+print(lc.open_file("main.py"))
+print("\n")
+"""
 
 # Run unit tests automatically
 #main(module='test_module', exit=False)
